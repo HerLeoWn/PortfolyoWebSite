15fd0156f73c29332240a22c155feb45ed5b87c2
HerLeoWn <mesutcanyazkan@gmail.com>
2024-12-10 19:52:32 +0300

    about - contact ejs oluşturuldu

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get("/", (res,req)=>{
     req.render("index.ejs")
 })
 
+app.get("/about", (res,req) =>{
+    req.render("about.ejs");
+})
+
+app.get("/contact", (res,req)=>{
+    req.render("contact.ejs");
+})
+
 app.listen((port), () =>{
     console.log(`Server running on port ${port}`);
 })
